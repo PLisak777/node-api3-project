8afc7abfbdefbcf10253aadcd6e71c06af9bc999
Phil <philip-lisak@lambdastudents.com>
2020-11-04 19:03:07 -0700

    logger function complete

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
-
 const server = express();
 
+server.use(logger);
+server.use(express.json());
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+	console.log(
+		`method: ${req.method}, request url: ${
+			req.originalUrl
+		}, time of request: ${new Date().toISOString()}`
+	);
+	next();
+}
 
 module.exports = server;
