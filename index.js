c6ce675088091b1715851d9905a1d0abc7bc4bc8
Phil <philip-lisak@lambdastudents.com>
2020-11-04 19:15:20 -0700

    Custom middleware functions complete on userRouter.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const server = require('./server');
+const userRouter = require('./users/userRouter');
+
+server.use('/api/user', userRouter)
+
+const PORT = 4000;
+
+server.listen(PORT)
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const db = require('./userDb')
+const postDb = require('../posts/postDb')
+const postRouter = require('../posts/postRouter')
 
 const router = express.Router();
+router.use('/:id', validateUserId)
+router.use('/:id/posts', postRouter)
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser (req, res) => {
+  
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -33,15 +38,41 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const userId = req.params.id ? req.params.id : -1
+  db.getById(userId)
+    .then(response => {
+      if (!response) {
+        res.status(400).json({ message: "invalid user id" })
+      } else {
+        req.user = response
+        next()
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({message: "Something went wrong!"})
+    })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing user data" })
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing post data" })
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" })
+  } else {
+    req.body.user_id = req.params.id
+    next()
+  }
 }
 
 module.exports = router;
